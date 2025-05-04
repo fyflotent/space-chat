8532dbbb95626250599538bda202aad1dedc3fc1
Chris Rice <chris.189.r@gmail.com>
2025-05-03 23:27:53 -0400

    Initial spacetime messaging example

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,34 +1,103 @@
-use spacetimedb::{ReducerContext, Table};
+use spacetimedb::{reducer, table, Identity, ReducerContext, Table, Timestamp};
 
-#[spacetimedb::table(name = person)]
-pub struct Person {
-    name: String
+#[table(name = user, public)]
+pub struct User {
+    #[primary_key]
+    identity: Identity,
+    name: Option<String>,
+    online: bool,
 }
 
-#[spacetimedb::reducer(init)]
-pub fn init(_ctx: &ReducerContext) {
-    // Called when the module is initially published
+#[table(name = message, public)]
+pub struct Message {
+    sender: Identity,
+    sent: Timestamp,
+    text: String,
 }
 
-#[spacetimedb::reducer(client_connected)]
-pub fn identity_connected(_ctx: &ReducerContext) {
-    // Called everytime a new client connects
-}
-
-#[spacetimedb::reducer(client_disconnected)]
-pub fn identity_disconnected(_ctx: &ReducerContext) {
-    // Called everytime a client disconnects
-}
-
-#[spacetimedb::reducer]
-pub fn add(ctx: &ReducerContext, name: String) {
-    ctx.db.person().insert(Person { name });
-}
-
-#[spacetimedb::reducer]
-pub fn say_hello(ctx: &ReducerContext) {
-    for person in ctx.db.person().iter() {
-        log::info!("Hello, {}!", person.name);
+/// Takes a name and checks if it's acceptable as a user's name.
+fn validate_name(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Names must not be empty".to_string())
+    } else {
+        Ok(name)
     }
-    log::info!("Hello, World!");
+}
+
+#[reducer]
+/// Clients invoke this reducer to set their user names.
+pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
+    let name = validate_name(name)?;
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User {
+            name: Some(name),
+            ..user
+        });
+        log::info!("A user set their name");
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+/// Takes a message's text and checks if it's acceptable to send.
+fn validate_message(text: String) -> Result<String, String> {
+    if text.is_empty() {
+        Err("Messages must not be empty".to_string())
+    } else {
+        Ok(text)
+    }
+}
+
+#[reducer]
+/// Clients invoke this reducer to send messages.
+pub fn send_message(ctx: &ReducerContext, text: String) -> Result<(), String> {
+    let text = validate_message(text)?;
+    log::info!("Message sent");
+    ctx.db.message().insert(Message {
+        sender: ctx.sender,
+        text,
+        sent: ctx.timestamp,
+    });
+    Ok(())
+}
+
+#[reducer(client_connected)]
+// Called when a client connects to a SpacetimeDB database server
+pub fn client_connected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        // If this is a returning user, i.e. we already have a `User` with this `Identity`,
+        // set `online: true`, but leave `name` and `identity` unchanged.
+        ctx.db.user().identity().update(User {
+            online: true,
+            ..user
+        });
+    } else {
+        // If this is a new user, create a `User` row for the `Identity`,
+        // which is online, but hasn't set a name.
+        ctx.db.user().insert(User {
+            name: None,
+            identity: ctx.sender,
+            online: true,
+        });
+        log::info!("Created user");
+    }
+}
+
+#[reducer(client_disconnected)]
+// Called when a client disconnects from SpacetimeDB database server
+pub fn identity_disconnected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User {
+            online: false,
+            ..user
+        });
+    } else {
+        // This branch should be unreachable,
+        // as it doesn't make sense for a client to disconnect without connecting first.
+        log::warn!(
+            "Disconnect event for unknown user with identity {:?}",
+            ctx.sender
+        );
+    }
 }
